1ae2b627c7b5945f51a664e6a686863732b18bdd
Anna Tostanovskaya <relanwo@relanwo.com>
2023-01-17 20:49:10 +0300

    #1.1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,13 +4,12 @@ import { Layout, Pagination, Card, Button } from 'antd';
 import PostsList from '../posts-list/posts-list';
 import CustomPagination from '../custom-pagination/custom-pagination';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
 	return (
-		<Router>
 			<div className={style['app']}>
 				<Header className={style['header']}>
 					<p className={style['logo']}>Realworld Blog</p>
@@ -22,14 +21,13 @@ function App() {
 						<Button className={style['sign-up']}>Sign Up</Button>
 					</div>
 				</Header>
-				<Content className={style['content']}></Content>
+				<Content className={style['content']}>
 					<PostsList />
 					<Footer className={style['footer']}>
 						<CustomPagination />
 					</Footer>
 				</Content>
 			</div>
-		</Router>
 	);
 }
 
